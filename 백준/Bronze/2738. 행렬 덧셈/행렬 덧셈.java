1fd7f121c9528bec20a2ebd64d994627fd719b59
noyes5 <116651434+noyes5@users.noreply.github.com>
2022-12-21 20:08:50 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 632 ms, Memory: 36108 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        int[][] firstArr = new int[row][col];
+        int[][] secondArr = new int[row][col];
+        int[][] answer = new int[row][col];
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                firstArr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                secondArr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                answer[i][j] = firstArr[i][j] + secondArr[i][j];
+            }
+        }
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                System.out.print(answer[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
