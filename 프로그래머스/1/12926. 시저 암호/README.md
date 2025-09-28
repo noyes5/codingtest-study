ff4181b7821dd7ce40c80f602101dc0567d2c369
Im sang hyun <116651434+noyes5@users.noreply.github.com>
2025-09-28 20:04:33 +0900

    [level 1] Title: 시저 암호, Time: 1.34 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,6 +1,6 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=java) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 28일 20:04:20
+2025년 09월 28일 20:04:34
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,18 +1,6 @@
 class Solution {
     public String solution(String s, int n) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c >= 'A' && c <= 'Z') {
-                c = (char) ((c - 'A' + n) % 26 + 'A');
-            }
-            
-            if (c >= 'a' && c <= 'z') {
-                c = (char) ((c - 'a' + n) % 26 + 'a');
-            }
-            
-            sb.append(c);
-        }
-        
-        return sb.toString();
+        String answer = "";
+        return answer;
     }
 }
